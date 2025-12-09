2561a956015c5ba9b26ed734fb842b69618f45f4
std::enable_if <std@enable-if.com>
2025-12-10 05:30:46 +0800

    feat: introduce new "fluent design"

--- src/config.ts
+++ src/config.ts
@@ -15,10 +15,10 @@ export const siteConfig: SiteConfig = {
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 110, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: false, // Hide the theme color picker for visitors
+		fixed: true, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: true,
+		enable: false, // Disabled - using Fluent background instead
 		src: "assets/images/banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		usePostCover: true, // If true, use post cover (page banner prop) when provided.
 		position: "bottom", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
